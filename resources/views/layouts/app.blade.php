e375d2b6175d33d8684180d2dad0eafd92f00297
musfiqur.emat <musfiqur.emat@gmail.com>
2019-08-24 12:22:47 +0600

    Test Push

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -53,8 +53,8 @@
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }} <span class="caret"></span>
-                                    </a>
-                                    <h1>Here</h1>
+                                </a>
+                                    
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
                                     <a class="dropdown-item" href="{{ route('logout') }}"
